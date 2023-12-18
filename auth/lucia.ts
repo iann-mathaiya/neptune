56106be523764d9134d96cc1d23bfc9e26157298
Ian Mathaiya <iannmathaiya@gmail.com>
2023-12-18 14:24:40 +0300

    setting up lucia auth

---
+++ auth/lucia.ts
@@ -0,0 +1,20 @@
+import { lucia } from "lucia"
+import { db } from "@vercel/postgres"
+import { nextjs_future } from "lucia/middleware"
+import { pg } from "@lucia-auth/adapter-postgresql"
+
+export const auth = lucia({
+  adapter: pg(db, {
+    key: "user_key",
+    user: "auth_user",
+    session: "user_session",
+  }),
+  env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
+  middleware: nextjs_future(),
+
+  sessionCookie: {
+    expires: false,
+  },
+})
+
+export type Auth = typeof auth
